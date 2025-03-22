d639d72fdb199d00e092359a62eac58055ceef7b
Frederick Villa <frederickalf002@gmail.com>
2025-03-22 23:52:28 +0800

    variables done

---
+++ variables/app.py
@@ -0,0 +1,10 @@
+price = 10 #integer
+rating = 4.9 #float
+name = "Fred" #string
+is_published = True #boolean
+print(price)
+
+#EXERCISE
+full_name = "John Smith" #string
+age = 20 #integer
+is_new_patient = True #boolean
