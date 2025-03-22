96e3a24d89552e81a3409dc383c1be1c3c1f0b79
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 00:58:25 +0800

    strings done

---
+++ strings/app.py
@@ -0,0 +1,27 @@
+# course = "Python's Course for Beginners" #Double quotes
+# course = 'Pythons Course for "Beginners"' #Single quotes
+# course = '''
+# # Hi John
+# #
+# # Here is our first email to you
+# #
+# # Thank you,
+# # The support team
+# #
+# # ''' #Triple quotes
+
+# course = 'Pythons Course for Beginners' #Single quotes
+# another = course[:]
+        # 0123456789...
+        # -1 is the last character
+        # -2 the second character from the end
+        #[0:3] = Pyt
+        #[0:] = return all the characters till the end of string
+        #[1:] = first character is removed
+        #[:5] = Pytho
+        #[:] = get all characters
+# print(another)
+
+# Exercise
+# name = 'Jennifer'
+# print(name[1:-1]) = ennife
