046898d20d6210d3c6ab37da769cde7aab5bfcce
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 01:33:19 +0800

    operator precedence done

---
+++ Operator Precedence/app.py
@@ -0,0 +1,13 @@
+# x = 10 + 3 * 2 #output = 16
+# x = 10 + 3 * 2 ** 2 #output = 22
+# x = (10 + 3) * 2 ** 2 #output = 52
+# print(x)
+# parenthesis - it always takes priority
+# exponentiation 2 ** 3
+# multiplication or division
+# addition or subtraction
+
+
+#Exercise
+# x = (2 + 3) * 10 - 3 #output = 47
+# print(x)
