c547d7902978638efae92a9c0144deb3b6008814
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 01:41:17 +0800

    math function done

---
+++ math functions/app.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.ceil(2.9)) #round up the number
+print(math.floor(2.9)) # round down the number
+x = 2.9
+print(round(x)) #round up the number
+print(abs(-x)) #absolute always returns a positive numbe
