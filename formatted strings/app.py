258ef7c01418fb18532128774e9cf282d5101e45
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 01:04:42 +0800

    formatted strings done

---
+++ formatted strings/app.py
@@ -0,0 +1,5 @@
+first_name = "Frederick"
+last_name = "Villa"
+message = first_name + ' [' + last_name + '] is a coder' #Concatination
+msg = f'{first_name} [{last_name}] is a coder' #formatted string
+print(msg)
