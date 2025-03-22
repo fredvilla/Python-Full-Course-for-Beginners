82d75793e296f8b0924f7045bc231a65b68b88c2
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 02:02:30 +0800

    if statement done

---
+++ if statements/app.py
@@ -0,0 +1,25 @@
+is_hot = False
+is_cold = False
+
+
+if is_hot:
+    print("It's a hot dayDrink plenty of water")
+    print("Drink plenty of water")
+elif  is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day!")
+print("Enjoy your day")
+
+# Exercise
+
+house_price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = house_price * 0.1
+
+else:
+    down_payment = house_price * 0.2
+print(f'Down payment: ${down_payment}')
