1a522e12a1a7e3861d4999caee62f06802db5f85
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 00:05:31 +0800

    receiving input done

--- getting input/app.py
+++ getting input/app.py
@@ -7,4 +7,4 @@ print('Hi ' + name) #Expression - it's a piece of code that produces a value
 full_name = input('What is your name? ')
 favorite_color = input ('What is your favorite color? ')
 
-print(full_name + ' likes ' + favorite_color)
+print('Hi ' + full_name + ' likes ' + favorite_color)
