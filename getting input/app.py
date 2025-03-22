28d6688058f11e83d7da26cfbed550b47c724fa0
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 00:03:08 +0800

    variables done

---
+++ getting input/app.py
@@ -0,0 +1,10 @@
+name = input('What is your name? ') #This is an input
+
+print('Hi ' + name) #Expression - it's a piece of code that produces a value
+
+#EXERCISE
+
+full_name = input('What is your name? ')
+favorite_color = input ('What is your favorite color? ')
+
+print(full_name + ' likes ' + favorite_color)
