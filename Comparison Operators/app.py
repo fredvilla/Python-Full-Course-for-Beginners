4430eab7ca99142871c904d67179e22befea8ec4
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 02:30:07 +0800

    comparison operators done

---
+++ Comparison Operators/app.py
@@ -0,0 +1,18 @@
+# temperature = 30
+#
+# if temperature >= 30:
+#     print(f"it's a hot day")
+# elif temperature <= 10:
+#     print(f"it's a cold day")
+# else:
+#     print(f"it's neither hot nor cold")
+
+
+name = "Frederick Villa"
+
+if len(name) <= 3:
+    print(f"name must be at least 3 characters")
+elif len(name) >= 50:
+    print(f"name can be a maximum of 50 characters")
+else:
+    print(f"name looks good!")
