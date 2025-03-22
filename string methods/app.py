6fee36a11317c25f7a04ceb9bbeb88e63d577d3c
Frederick Villa <frederickalf002@gmail.com>
2025-03-23 01:19:44 +0800

    strings methods done

---
+++ string methods/app.py
@@ -0,0 +1,14 @@
+course = 'Python for Beginners'
+
+# print(len(course)) = to count the number of characters into string
+# print(course.upper()) #converting string into uppercase
+# print(course.lower()) #converting string into lowercase
+print(course.title())
+
+# print(course.find('P')) #character finder and it converts into integer
+
+# print(course.replace('P', 'J')) #replacing a character
+
+#-Boolean Expression-
+# print('Python' in course) #checking the character if it is in the value it can tell if  this is True or False
+
